410bcd0f2d13627555b1c72499037fd5e0b605f5
Parichay Kapoor <pk.kapoor@samsung.com>
2019-04-15 15:01:05 +0900

    Added Valgrind as an option
    Valgrind can be enabled by passing --enable-valgrind to ssat
    Currently, --full-leaks and --suppressions are not enabled for valgrind
    
    Signed-off-by: Parichay Kapoor <pk.kapoor@samsung.com>

--- ssat-api.sh
+++ ssat-api.sh
@@ -264,7 +264,11 @@ function callCompareTest() {
 ## @param $4 set 1 if this passes if gstLaunch fails.
 ## @param $5 set 1 to enable PERFORMANCE test.
 function gstTest() {
-	calloutput=$(gst-launch-1.0 -f -q $1)
+	if [[ "$VALGRIND" -eq "1" ]]; then
+		calloutputprefix='valgrind --track-origins=yes'
+	fi
+	calloutput=$(eval $calloutputprefix gst-launch-1.0 -f -q $1)
+
 	retcode=$?
 	desired=0
 	if [[ "${4}" -eq "1" ]]; then
